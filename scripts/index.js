4b9ebd0b99dc39b9a44694c56823bbdd85373e17
אלמג מהנדסי קרינה <moreladu@gmaile.com>
2023-12-06 10:30:39 +0200

    Json

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,32 @@
-
+let response = [];
 
 
 let xhr = new XMLHttpRequest();
 xhr.open("GET", "../data/users.json", true);
 xhr.onload = function () {
-    let response = JSON.parse(this.responseText);
+    response = JSON.parse(this.responseText);
+    response = response.users;
     console.log(response);
-
+    const login_handler = () => {
+        let flag = false;
+        let uname = document.getElementById("uname");
+        let password = document.getElementById("password");
+        if (uname.value == "" || password.value == "") {
+            alert("MISSING DATA! PLEASE FILL ALL FIELDS")
+        }
+        for (let x in response) {
+            if (uname.value == response[x].username && password.value == response[x].password) {
+                flag = true;
+                localStorage.setItem("current", JSON.stringify(response[x]));
+            }
+        }
+        if (flag == true) {
+            localStorage.setItem("users", JSON.stringify(response));
+            window.location.href = "../pages/home.html";
+        } else {
+            alert("THE DATA ENTERED DOES NOT EXIST! PLEASE TRY AGAIN.")
+        }
+    }
+    document.getElementById("btn").addEventListener("click", login_handler)
 }
 xhr.send()
